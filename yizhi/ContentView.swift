f8421aafd2b97dd47e762d045843dcaf575a29ef
Devon Crebbin <devon@land.org.au>
2024-12-18 00:24:03 +1030

    starting concept

--- yizhi/ContentView.swift
+++ yizhi/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    struct Task {
+        let name: String
+        var completed: Bool
+    }
+
+    @State var tasks = [
+        Task(name: "task1", completed: false),
+        Task(name: "task2", completed: false),
+        Task(name: "task3", completed: false),
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Text("一只 yīzhí").font(.system(size: 30))
+            ScrollView {
+                Text("今天 / today").font(.system(size: 20))
+                ForEach(Array(tasks.enumerated()), id: \.offset) { index, task in
+                    HStack {
+                        Text(task.name)
+                        Spacer()
+                        //checkbox
+                        Button(action: {
+                            tasks[index].completed.toggle()
+                        }) {
+                            Image(
+                                systemName: tasks[index].completed
+                                    ? "checkmark.square.fill" : "square"
+                            ).font(.system(size: 24))
+                                .foregroundColor(.black)
+                        }
+                    }.padding(.horizontal, 20).padding(.vertical, 10)
+                }
+            }.frame(maxWidth: .infinity, maxHeight: 200)
+            Spacer()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.top, 10)
     }
 }
 
